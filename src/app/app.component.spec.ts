1f8a9c8e52c50367b00d758aea90b96398f4cd1a
pearl-nguyen <pearl.nguyen@nrcan-rncan.gc.ca>
2025-03-19 08:33:24 -0400

    fix test to work with karma

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,21 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { CommonModule } from '@angular/common';
-import { BrowserModule } from '@angular/platform-browser';
-import { provideRouter, RouterModule, RouterOutlet } from '@angular/router';
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { provideRouter, RouterModule } from '@angular/router';
+import { AppModule } from './app.module';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
+
     beforeEach(() => TestBed.configureTestingModule({
-      imports: [RouterModule.forRoot([])], // Ensure RouterModule is imported
+      imports: [RouterModule.forRoot([]), AppModule],
       declarations: [AppComponent],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA],
-      providers: [
-        provideRouter([])
-      ]
     }).compileComponents());
-  });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
